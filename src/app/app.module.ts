a6f15bd0be768eaa43b6f65d579b4417c60f796c
acviji <acvijikumar@gmail.com>
2017-04-07 18:42:12 +0530

    Routes menu creation
    menu creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,28 @@ import { HttpModule }    from '@angular/http';
 
 //Componets
 import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
-import { FirstComponent } from './first/first.component';
+import { IndexComponent } from './home/index.component';
+import { FirstComponent } from './about/first.component';
 
 //service
 import { firstServiceJsonData } from './service/drinks_list';
+import { ProductComponent } from './product/product.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 const vijayroutes: Routes = [
   {path:'' , redirectTo :'link1', pathMatch:'full'},
-  { path: 'link1', component: IndexComponent },
-  { path: 'link2', component: FirstComponent }
+  { path: 'home', component: IndexComponent },
+  { path: 'about', component: FirstComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'contact-us', component: ContactComponent }
+  
 ];
 
 @NgModule({
-  declarations: [AppComponent,IndexComponent,FirstComponent],
+  declarations: [AppComponent,IndexComponent,FirstComponent, ProductComponent, ServicesComponent, ContactComponent],
   imports: [BrowserModule, HttpModule,  RouterModule.forRoot(vijayroutes) ],
   providers: [firstServiceJsonData],
   bootstrap: [AppComponent]
